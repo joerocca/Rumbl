577d6c57c44a4edc928f5e826b3d33e277a92ea7
Joe Rocca <jprocca813@gmail.com>
2016-09-10 01:01:14 -0400

    Added video channel for annotations

--- web/static/js/video.js
+++ web/static/js/video.js
@@ -2,7 +2,8 @@ import Player from "./player"
 
 let Video = {
 
-	init(socket, element){ if(!element){return}
+	init(socket, element){ 
+		if(!element){return}
 		let playerId = element.getAttribute("data-player-id")
 		let videoId = element.getAttribute("data-id")
 		socket.connect()
@@ -14,44 +15,43 @@ let Video = {
 	onReady(videoId, socket){
 		let msgContainer = document.getElementById("msg-container")
 		let msgInput = document.getElementById("msg-input")
+		let postButton = document.getElementById("msg-submit")
 		let vidChannel = socket.channel("videos:" + videoId)
-		// vidChannel.on("ping", ({count}) => console.log("PING", count))
+
 		postButton.addEventListener("click", e => {
 			let payload = {body: msgInput.value, at: Player.getCurrentTime()}
 			vidChannel.push("new_annotation", payload)
-					  .receive("error", e => console.log(e))
+			.receive("error", e => console.log(e))
 			msgInput.value = ""
 		})
 
-		vidChannel.on("new_annotation", (resp) => {
+		vidChannel.on('new_annotation', (resp) => {
 			this.renderAnnotation(msgContainer, resp)
 		})
 
 		vidChannel.join()
 			.receive("ok", resp => console.log("joined the video channel", resp))
 			.receive("error", reason => console.log("join failed", reason))
+	},
 
-		esc(str){
-			let div = document.createElement("div")
-			div.appendChild(document.createTextNode(str))
-			return div.innerHTML
-		},
+	esc(str){
+		let div = document.createElement("div")
+		div.appendChild(document.createTextNode(str))
+		return div.innerHTML
+	},
 
-		renderAnnotation(msgContainer, {user, body, at}){
-			//TODO append annotation to msgContainer
-			let template = document.createElement("div")
+	renderAnnotation(msgContainer, {user, body, at}){
+		//TODO append annotation to msgContainer
+		let template = document.createElement("div")
 
-			template.innerHTML = `
+		template.innerHTML = `
+		<a href="#" data-seek="${this.esc(at)}">
+			<b>${this.esc(user.username)}</b>: ${this.esc(body)}
+		</a>`
 
-			<a href="#" data-seek="${this.esc(at)}">
-				<b>${this.esc(user.username)}</b>: ${this.esc(body)}
-			</a>
-			`
-
-			msgContainer.appendChild(template)
-
-			msgContainer.scrollTop = msgContainer.scrollHeight
-		}
+		msgContainer.appendChild(template)
+		msgContainer.scrollTop = msgContainer.scrollHeight
 	}
+		
 }
 export default Video
